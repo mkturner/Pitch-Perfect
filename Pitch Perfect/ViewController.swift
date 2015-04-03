96cec28172bd8a3d8ba462011f5695cdfe14e329
Marvin T <contact@marvinkturner.com>
2015-04-02 20:32:44 -0400

    Pitch Perfect beta - First commit
    This is a working version of the App as of the end of Lesson two of
    "Intro to iOS App Development with Swift".

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        // TODO: Show text "recording in progress"
+        recordingInProgress.hidden = false
+        // TODO: Record the user's voice
+        println("in recordAudio")
+    }
 
+    @IBAction func doneRecording(sender: UIButton) {
+        // TODO: Hide "recording in progress"
+        recordingInProgress.hidden = true
+        // TODO: Save and store Recorded audio
+    }
 }
 
